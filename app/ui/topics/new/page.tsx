9d242ede8a12e5fa93203928159bed92de05b6a6
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-31 16:37:53 -0600

    add form to new page

--- app/ui/topics/new/page.tsx
+++ app/ui/topics/new/page.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import CreateTopicForm from "@/components/CreateTopicForm";
 
-export default function page() {
-  return <div>New Topics Page</div>;
+export default function Page() {
+  return (
+    <div>
+      <h1 className={`mb-4 text-xl md:text-2xl`}>New Topic</h1>
+      <CreateTopicForm />
+    </div>
+  );
 }
