b996c3fe9005ddb140c0baa7a4d347242b738369
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-31 11:14:02 -0600

    make topiclinks client side for hook to highlight topic when on its page

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -2,6 +2,6 @@ import { resolve } from "path";
 import React from "react";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return <div>Topics Page: {params.id}</div>;
 }
