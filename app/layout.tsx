a164b1c99d23787c65bdcdc56707d2002c7091f3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-02-02 13:04:42 -0600

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/global.css";
 import { inter } from "@/app/fonts";
 import { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Full Stack Next.js | Atlas School",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: Props) {
     <html lang="en">
       <body className={`${inter.className} antialiased text-secondary`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
